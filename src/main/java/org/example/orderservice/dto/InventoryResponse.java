1f8a6975d3ff4aec33dc97d76cc7495efc525831
aamirxshaikh <aamirshaikh3232@outlook.com>
2024-03-02 13:46:28 +0530

    Enhance Order Service with Inventory Service Communication
    This commit introduces the following changes to the codebase:
    
    - Added a new DTO class `InventoryResponse` to represent the response from the inventory service.
    - Modified the `OrderService` class to incorporate communication with the inventory service using WebClient.
    - Introduced the usage of WebClient to make a GET request to the inventory service, checking the availability of items before placing an order.
    
    Purpose:
    The purpose of this commit is to enhance the Order Service functionality by integrating communication with the Inventory Service and handling inventory responses for better order processing.

---
+++ src/main/java/org/example/orderservice/dto/InventoryResponse.java
@@ -0,0 +1,15 @@
+package org.example.orderservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class InventoryResponse {
+  private String skuCode;
+  private boolean isInStock;
+}
